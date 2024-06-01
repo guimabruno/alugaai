b23b0c175f314a75c2be79e071be292b6fb77deb
Fabio <fabio.donizete@yahoo.com.br>
2024-06-01 13:22:47 -0300

    [ADD] criado banco de dados do cadastro de veiculos

--- app/src/main/java/com/example/alugai/BancoController.java
+++ app/src/main/java/com/example/alugai/BancoController.java
@@ -4,6 +4,9 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+
+import java.io.ByteArrayOutputStream;
 
 public class BancoController {
     private SQLiteDatabase db;
@@ -37,6 +40,36 @@ public class BancoController {
         else
             return "Dados cadastrados com sucesso!";
     }
+
+    public String insereDadosCarro(int idUser, String marca, String modelo, String cor, String placa, String renavam, Bitmap imagem) {
+        ContentValues valores;
+        long resultado;
+        db = banco.getWritableDatabase();
+
+        valores = new ContentValues();
+        valores.put("idUser", idUser);
+        valores.put("marca", marca);
+        valores.put("modelo", modelo);
+        valores.put("cor", cor);
+        valores.put("placa", placa);
+        valores.put("renavam", renavam);
+        valores.put("imagem", getBitmapAsByteArray(imagem));
+
+        resultado = db.insert("veiculos", null, valores);
+        db.close();
+
+        if (resultado == -1)
+            return "Erro ao inserir os dados do veículo!";
+        else
+            return "Dados do veículo cadastrados com sucesso!";
+    }
+
+    private byte[] getBitmapAsByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        return outputStream.toByteArray();
+    }
+
 
     // Consultar para ver se usuario existe
     public Cursor carregaDadosParaLogin(String _login, String _senha) {
--- app/src/main/java/com/example/alugai/CriaBanco.java
+++ app/src/main/java/com/example/alugai/CriaBanco.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class CriaBanco extends SQLiteOpenHelper {
 
     private static final String NOME_BANCO = "alugaAI.db";
-    private static final int VERSAO = 1;
+    private static final int VERSAO = 2;
     public CriaBanco(Context context) {
         super(context, NOME_BANCO, null, VERSAO);
     }
@@ -22,11 +22,24 @@ public class CriaBanco extends SQLiteOpenHelper {
                 + "telefone text,"
                 + "senha text)";
         db.execSQL(sql);
+
+        sql = "CREATE TABLE veiculos ("
+                + "idCarro integer primary key autoincrement,"
+                + "idUser integer,"
+                + "marca text,"
+                + "modelo text,"
+                + "cor text,"
+                + "placa text,"
+                + "renavam text,"
+                + "imagem blob,"
+                + "FOREIGN KEY (idUser) REFERENCES usuarios(idUser))";
+        db.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS usuarios");
+        db.execSQL("DROP TABLE IF EXISTS veiculos");
         onCreate(db);
     }
 }
