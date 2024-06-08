464f246d539e48ee030dc0a74e4a60354df8a43c
Fabio <fabio.donizete@yahoo.com.br>
2024-06-07 21:36:36 -0300

    [EDIT] Adicionado funcionalidade e estilo no Extras

--- app/src/main/java/com/example/alugai/Extras.java
+++ app/src/main/java/com/example/alugai/Extras.java
@@ -1,7 +1,9 @@
 package com.example.alugai;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Extras extends AppCompatActivity {
-
+    String nome, email, endereco, cpf, telefone;
+    int idUser;
     Button btMeusCarros, btHistContratos, btMensagens, btCarteira;
+
+    ImageView btVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,29 @@ public class Extras extends AppCompatActivity {
         btHistContratos = (Button) findViewById(R.id.btHistContratos);
         btMensagens = (Button) findViewById(R.id.btMensagens);
         btCarteira = (Button) findViewById(R.id.btCarteira);
+        btVoltar = (ImageView) findViewById(R.id.btVoltar);
+
+        Intent intencao = getIntent();
+        Bundle parametros = intencao.getExtras();
+        assert parametros != null;
+        idUser = parametros.getInt("idUser");
+        nome = parametros.getString("nome");
+        email = parametros.getString("email");
+        endereco = parametros.getString("endereco");
+        cpf = parametros.getString("cpf");
+        telefone = parametros.getString("telefone");
+
+        btVoltar.setOnClickListener(v -> {
+            Intent tela = new Intent(Extras.this, Menu.class);
+            parametros.putInt("idUser", idUser);
+            parametros.putString("nome", nome);
+            parametros.putString("email", email);
+            parametros.putString("endereco", endereco);
+            parametros.putString("cpf", cpf);
+            parametros.putString("telefone", telefone);
+            tela.putExtras(parametros);
+            startActivity(tela);
+        });
 
         };
     }
